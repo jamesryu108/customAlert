27a0e0061d0392b8c3b0e6eb1f2c34b52bf348a7
James <jamesryu108@gmail.com>
2020-08-11 23:46:34 -0400

    Initial Commit

---
+++ customAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  customAlert
+//
+//  Created by James Ryu on 2020-08-11.
+//  Copyright © 2020 James Ryu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
